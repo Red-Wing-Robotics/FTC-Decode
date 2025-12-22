ef4b8df58fc28e6facce14088da7b6dafc41b15f
David Tucker <david@davidtucker.net>
2025-12-22 08:01:42 -0500

    Fixing a warning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/org/firstinspires/ftc/teamcode/opmodes/RWRBaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/org/firstinspires/ftc/teamcode/opmodes/RWRBaseOpMode.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.util.log.Logger;
 
 abstract public class RWRBaseOpMode extends OpMode {
