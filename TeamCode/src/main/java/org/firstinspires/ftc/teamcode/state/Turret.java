ec3e50281c6f9928f5deac6c67a61f64bc474c43
David Tucker <david@davidtucker.net>
2026-01-24 14:16:45 -0500

    Adding in turret state machine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/state/Turret.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.state;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.util.log.Logger;
+
+public class Turret {
+
+    public static double TURRET_MIDPOINT = 0.5;
+    public static double FUDGE_FACTOR = 1.0;
+
+    public final Servo turret;
+    private final Logger logger;
+
+    public Turret(HardwareMap hardwareMap, Telemetry telemetry){
+        this.logger = new Logger(telemetry);
+        turret = hardwareMap.get(Servo.class, "turret");
+    }
+
+    public void update() {
+        setToDefault();
+    }
+
+    public void update(LLResult result) {
+        if (result != null && result.isValid()) {
+            double tx = result.getTx();
+            if(Math.abs(tx) < FUDGE_FACTOR) {
+                return;
+            }
+            double currentPosition = turret.getPosition();
+            turret.setPosition(currentPosition + (tx / 100));
+        } else {
+            setToDefault();
+        }
+    }
+
+    private void setToDefault() {
+        turret.setPosition(TURRET_MIDPOINT);
+    }
+
+}
